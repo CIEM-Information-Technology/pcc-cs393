5a4a5d24f7e200d4106678d73f3541e650c7ced1
aurghya-0 <aurghya.blog@gmail.com>
2022-07-14 09:09:38 +0530

    modified files'

--- Week_2/leap_year.py
+++ Week_2/leap_year.py
@@ -1,2 +1,9 @@
 def leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+if __name__ == '__main__':
+    year = int(input("Enter a year: "))
+
+    print(f"{year} is a leap year") if leap_year(year) else print(f"{year} is not a leap year")
+    
